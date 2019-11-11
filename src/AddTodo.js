2a20c142513cf3257d0e85275f1e8935e6c94142
gavin-crowley <gavin-crowley@users.noreply.github.com>
2019-11-11 18:24:24 +0400

    init

---
+++ src/AddTodo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddTodo = () => {
+  return (
+    <div className="todos collection">
+      
+    </div>
+  );
+}
+
+export default AddTodo;
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Todos from "./Todos";
+
+class App extends Component {
+  state = {
+    todos: [{ id: 1, content: "Buy milk" }, { id: 2, content: "Study React" }]
+  };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id != id
+    });
+    this.setState({
+      todos
+    })
+  }
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
